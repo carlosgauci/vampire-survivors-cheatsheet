b4a18be545d9e6264a2128fa10cc1de7988b3d7a
Carlos <53526597+carlosgauci@users.noreply.github.com>
2022-06-21 21:02:52 +0200

    modal width

--- components/InfoBox.tsx
+++ components/InfoBox.tsx
@@ -14,7 +14,7 @@ const InfoBox = ({ hoveredItem, selectedItem, setSelectedItem, modal, setShowMod
   const hovered = items.find((i) => i.name === hoveredItem);
   const selected = items.find((i) => i.name === selectedItem);
   return (
-    <section className={`relative card w-full max-w-sm lg:max-w-md p-6 flex-col gap-6 overflow-y-auto scrollbar ${modal ? "mx-auto flex h-full" : "m-0 hidden md:flex"}`}>
+    <section className={`relative card w-full  p-6 flex-col gap-6 overflow-y-auto scrollbar ${modal ? "mx-auto flex h-full max-w-md" : "m-0 hidden md:flex max-w-sm lg:max-w-md"}`}>
       {hovered ? <ItemInfo item={hovered} setSelectedItem={setSelectedItem} /> : selected ? <ItemInfo item={selected} setSelectedItem={setSelectedItem} /> : null}
       {modal && (
         <button onClick={() => setShowModal(false)} className="absolute top-0 right-0 p-4 text-xl leading-4">
